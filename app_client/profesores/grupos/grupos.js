7c513bacf3d46c7f7b157a2a9c8e1739829e5540
Joel Rodriguez <joelerll@gmail.com>
2017-03-29 23:09:02 -0500

    drag and drop

--- app_client/profesores/grupos/grupos.js
+++ app_client/profesores/grupos/grupos.js
@@ -4,22 +4,139 @@ var app = new Vue({
     reverseMessage: function () {
       this.message = this.message.split('').reverse().join('')
     },
-    obtenerTodosProfesores: function () {
+    obtenerTodosGrupos: function () {
       this.$http.get('/api/grupos').then(response => {
         this.grupos = response.body.datos;
+        this.obtenerTodosEstudiantes();
       }, response => {
+        // codigo error
       });
+    },
+    obtenerTodosEstudiantes: function() {
+      this.$http.get('/api/estudiantes').then(response => {
+        this.estudiantes = response.body.datos;
+        this.estudiantesNoEnGrupo()
+      }, response => {
+        // codigo error
+      });
+    },
+    estudiantesNoEnGrupo: function() {
+      let temp = []
+      this.estudiantesSinGrupo = []
+      this.grupos.forEach((grupo) => {
+        grupo.estudiantes.forEach((estudiante) => {
+          temp.push(estudiante._id);
+        })
+      })
+      this.estudiantes.forEach((es) => {
+        if (!temp.includes(es._id)) {
+          this.estudiantesSinGrupo.push(es)
+        }
+      })
+    },
+    anadirEstudianteAGrupo: function(index_grupo,estudiante) {
+      // this.grupos[index_grupo].estudiantes.push(estudiante)
+      // estudianteEstaEnGrupo(this.grupos[index_grupo], estudiante)
+      // this.grupos[index_grupo].estudiantes.push(estudiante)
+      let cont = 0
+      this.estudiantesSinGrupo.forEach(es => {
+        if (es._id == estudiante._id) {
+          this.estudiantesSinGrupo.splice(cont, 1)
+        }
+        cont = cont + 1
+      })
+
+      this.grupos.forEach(grupo => {
+        grupo.estudiantes.forEach(est => {
+          if (est._id === estudiante._id) {
+            this.$http.delete(`/api/grupos/${grupo._id}/estudiantes/${est._id}`).then(response => {
+              if (response.body.estado) {
+              }
+            }, response => {
+              // codigo error
+            });
+          } else {
+            // anadir estudiante a grupo
+            this.$http.post(`/api/grupos/${this.grupos[index_grupo]._id}/estudiantes/${estudiante._id}`).then(response => {
+              if (response.body.estado) {
+
+              }
+            }, response => {
+              // codigo error
+            });
+          }
+        })
+      })
+      this.obtenerTodosGrupos()
+
+
+      // eliminar estudiante de grupo
+
+    },
+    move: function(a) {
+      console.log(a)
     }
   },
 	data: {
       grupos: [
+      ],
+      estudiantesSinGrupo: [
+      ],
+      list2: [],
+      estudiantes: [
+
+      ],
+      nuevos: [
       ]
 	},
-
+  watch: {
+    nuevos: function(nuevo) {
+      console.log(nuevo)
+    }
+  },
 	mounted: function(){
 		$('.button-collapse').sideNav();
 	}
 
 });
 
-app.obtenerTodosProfesores()
+app.obtenerTodosGrupos()
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev, target) {
+    ev.preventDefault();
+    let estudianteA
+    var data = ev.dataTransfer.getData("text");
+    app.estudiantes.forEach((estudiante) => {
+      if (estudiante._id == data) {
+        estudianteA = estudiante
+      }
+    })
+    var cont = 0
+    app.grupos.forEach(grupo => {
+      if (grupo._id == target.id) {
+        app.anadirEstudianteAGrupo(cont,estudianteA)
+      }
+      cont = cont + 1
+    })
+    //ev.target.appendChild(document.getElementById(data));
+}
+
+
+function estudianteEstaEnGrupo(grupos, estudiante) {
+  grupos.forEach(grupo => {
+    grupo.estudiantes.forEach(est => {
+      if (est._id === estudiante._id) {
+        return true
+      }
+    })
+  })
+  return false
+}
