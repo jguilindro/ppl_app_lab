b71d35e435da145eaa8f899b2d01b41b1b580a36
edisonmora95 <edison_andre_9@hotmail.com>
2017-04-01 08:59:57 -0500

    profesores/nueva-pregunta completo sin wysiwyg

--- app_client/profesores/partials/navbar.vue
+++ app_client/profesores/partials/navbar.vue
@@ -1,16 +1,27 @@
-<nav class="nav-extended">
-  <div class="nav-wrapper">
-    <a href="#!" class="brand-logo">Logo</a>
-    <ul class="right hide-on-med-and-down">
-      <li><a>A link</a></li>
-      <li><a>A second link</a></li>
-      <li><a>A third link</a></li>
-    </ul>
-  </div>
-  <div class="nav-content">
-    <span class="nav-title">Title</span>
-    <a class="btn-floating btn-large halfway-fab waves-effect waves-light teal">
-      <i class="material-icons">add</i>
-    </a>
-  </div>
-</nav>
+<template>
+  <nav id="navbar">
+    <div class="nav-wrapper">
+      <a href="../grupos" class="brand-logo titulo-nav">Profesor</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down ">
+        <li><a href="#">Sesiones</a></li>
+        <li><a href="#">Lecciones</a></li>
+        <li><a href="../grupos">Grupos</a></li>
+        <li><a href="../preguntas">Preguntas</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a href="#">Sesiones</a></li>
+        <li><a href="#">Lecciones</a></li>
+        <li><a href="../grupos">Grupos</a></li>
+        <li><a href="../preguntas">Preguntas</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'navbar'
+  }
+</script>
+
--- app_client/profesores/preguntas/nueva-pregunta/nuevaPreguntaScript.js
+++ app_client/profesores/preguntas/nueva-pregunta/nuevaPreguntaScript.js
@@ -1,11 +1,52 @@
-  
-  $(document).ready(function() {
-    $('select').material_select();
-  });
+
+var app = new Vue({
+	mounted: function(){
+		$('.button-collapse').sideNav();
+		$('select').material_select();
+		$('.chips').material_chip();
+		$('.chips-placeholder').material_chip({
+		    placeholder: 'Enter a tag',
+		    secondaryPlaceholder: 'Enter a Tag',
+		});
 
 
-  $('.chips').material_chip();
-   $('.chips-placeholder').material_chip({
-    placeholder: 'Enter a tag',
-    secondaryPlaceholder: 'Enter a Tag',
-  });
+	},
+	el: '#preguntaNueva',
+	data: {
+		selected: 'sad',
+		pregunta: {
+			titulo: '',
+			tags: [],
+			descripcion: '',
+			imagenes: [],	//Es opcional añadir imágenes a la pregunta
+			tipoPregunta: '',	//V/F, justifiacación u opción múltiple
+			opciones: [],		//Se llena solo si tipoPregunta=='Opcion multiplie'
+			tipoLeccion: '',	// Lección, tutorial o laboratorio
+			tiempoEstimado: 0,
+			tiempoBandera: 0,	// Tiempo en que la bandera cambiará de color para el Real Time
+			creador: '',		//Se deberia llenar con las sesiones, trabajo de Julio Guilindro
+			capitulo: '',		//Se llena solo si tipoLeccion=='leccion'
+			tutorial: '',		//Se llena solo si tipoLeccion=='tutorial'
+			laboratorio: '',	//Se llena solo si tipoLeccion=='Laboratorio'
+			puntaje: 0
+
+		}
+	}
+});
+
+
+$('#tipo-leccion').change(function(){ 
+	//app.set('select', $('#jurisdiction').val()); 
+	//console.log( 'Text: ' + $('#tipo-leccion option:selected').text())
+	//console.log('Antes: ' + app.$data.pregunta.tipoLeccion)
+	app.$data.pregunta.tipoLeccion = $('#tipo-leccion option:selected').text();
+	//console.log('Despues: ' + app.$data.pregunta.tipoLeccion)
+});
+
+$('#tipo-pregunta').change(function(){ 
+	//app.set('select', $('#jurisdiction').val()); 
+	//console.log( 'Text: ' + $('#tipo-pregunta option:selected').text())
+	//console.log('Antes: ' + app.$data.pregunta.tipoPregunta)
+	app.$data.pregunta.tipoPregunta = $('#tipo-pregunta option:selected').text();
+	//console.log('Despues: ' + app.$data.pregunta.tipoPregunta)
+});
