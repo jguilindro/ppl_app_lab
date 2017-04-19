154e75329fb2a7f2eb43e193c8ced375de68e6a4
edisonmora95 <edison_andre_9@hotmail.com>
2017-04-18 21:59:51 -0500

    Minor fixes

--- app_client/estudiantes/perfil/estudiante.js
+++ app_client/estudiantes/perfil/estudiante.js
@@ -1,6 +1,10 @@
 var app = new Vue({
   el: '#app',
+  mounted: function(){
+    this.obtenerLogeado();
+  },
   data: {
+    estudiante: {}/*
     estudiante: {
     	nombre: 'Edison',
     	apellido: 'Mora',
@@ -24,11 +28,21 @@ var app = new Vue({
     			fecha: '7-06-2017'
     		}
     	]
-    }
+    }*/
   },
   methods: {
     tomarLeccion : function(){
         window.location.href = '/estudiantes/tomar-leccion'
+    },
+    obtenerLogeado: function() {
+      var self = this;
+      this.$http.get('/api/session/usuario_conectado').
+        then(res => {
+          if (res.body.estado) {
+            self.estudiante = res.body.datos;
+            console.log(self.estudiante)
+          }
+        });
     }
   }
-})
+});
