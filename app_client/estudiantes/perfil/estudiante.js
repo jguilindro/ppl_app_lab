9ded1707dc0f1b1528ba36165ff8fe5244ad1de6
edisonmora95 <edison_andre_9@hotmail.com>
2017-04-18 22:28:24 -0500

    Mejorado perfil de estudiantes

--- app_client/estudiantes/perfil/estudiante.js
+++ app_client/estudiantes/perfil/estudiante.js
@@ -6,28 +6,7 @@ var app = new Vue({
   data: {
     estudiante: {}/*
     estudiante: {
-    	nombre: 'Edison',
-    	apellido: 'Mora',
-    	lecciones: [
-    		{
-    			nombre: 'Leccion 1',
-    			estado: 'Calificado',
-    			calificacion: '80',
-    			fecha: '21-05-2017'
-    		},
-    		{
-    			nombre: 'Leccion 2',
-    			estado: 'Calificado',
-    			calificacion: '60',
-    			fecha: '29-05-2017'
-    		},
-    		{
-    			nombre: 'Leccion 3',
-    			estado: 'Pendiente',
-    			calificacion: '',
-    			fecha: '7-06-2017'
-    		}
-    	]
+    	
     }*/
   },
   methods: {
