f1fc3eedc379e5bc9c2a0422c19922002cfcda37
Joel Rodriguez <joelerll@gmail.com>
2017-04-20 08:49:35 -0500

    realtime obtener pregunta estudiante

---
+++ app_api/models/grupoLeccion.model.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+mongoose.Promise = global.Promise;
+
+const GrupoLeccionSchema = mongoose.Schema({
+	_id: {
+		type: String,
+		unique: true,
+		'default': shortid.generate
+	},
+  grupo: {
+    type: String,
+    'ref': 'Grupo'
+  },
+  paralelo: {
+    type: String,
+    ref: 'Paralelo'
+  },
+  leccion: {
+    type: String,
+    ref: 'Leccion'
+  },
+  tiempoBono: { // tiempo en minutos
+    type: Number
+  },
+  preguntaActual: {
+    type: Number,
+    'default': 1
+  },
+  fechaEmpezado:{
+    type: Date
+  },
+  fechaTerminado: {
+    type: Date
+  },
+  participantes: [{
+    estudiante: {
+      type: String,
+      ref: 'Estudiante',
+      unique: true
+    },
+    estudianteSocketId: {
+      type: String
+    },
+    contestadoPreguntaActual: {
+      type: Boolean,
+      'default': false
+    }
+  }]
+},{versionKey: false, timestamps: true, collection: 'grupoLeccion'})
+
+GrupoLeccionSchema.methods.crearGrupoLeccion = function(callback) {
+  this.save(callback)
+}
+
+GrupoLeccionSchema.statics.anadirParticipante = function(id_grupo, id_leccion, id_estudiante, callback) {
+  let participante = {
+    estudiante: id_estudiante
+  }
+  this.update({$and: [{grupo: id_grupo}, {leccion: id_leccion}]},{$addToSet: {'participantes': participante}}, callback)
+}
+
+GrupoLeccionSchema.statics.participanteExiste = function(id_grupo, id_leccion, id_estudiante, callback) {
+  this.findOne({$and: [{grupo: id_grupo}, {leccion: id_leccion}, {'participantes.estudiante': id_estudiante}]}, callback)
+}
+
+GrupoLeccionSchema.statics.obtenerGrupoLeccion = function(id_grupo, id_leccion, callback) {
+  this.findOne({$and: [{grupo: id_grupo}, {leccion: id_leccion}]}, callback)
+}
+
+module.exports = mongoose.model('GrupoLeccion', GrupoLeccionSchema)
