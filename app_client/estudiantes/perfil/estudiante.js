ffa5ce5d5cc40bc0f3178665f31afc53b4ddfcf0
edisonmora95 <edison_andre_9@hotmail.com>
2017-04-26 12:48:09 -0500

    Arreglado problema en perfil del estudiante
    Habían lecciones null en el objeto del estudiante.
    Verificar por qué ocurrió eso.
    Tal vez alguien eliminó las lecciones de la base de datos

--- app_client/estudiantes/perfil/estudiante.js
+++ app_client/estudiantes/perfil/estudiante.js
@@ -4,10 +4,8 @@ var app = new Vue({
     this.obtenerLogeado();
   },
   data: {
-    estudiante: {}/*
-    estudiante: {
-    	
-    }*/
+    estudiante: {},
+    lecciones: []
   },
   methods: {
     tomarLeccion : function(){
@@ -19,13 +17,30 @@ var app = new Vue({
         then(res => {
           if (res.body.estado) {
             self.estudiante = res.body.datos;
-            console.log(self.estudiante)
+            console.log('Este es el estudiante conectado')
+            console.log(self.estudiante);
+            self.presentarLecciones();
           }
         });
     },
     verLeccion: function(event){
       leccion = event.currentTarget.id;
       window.location.href = '/estudiantes/ver-leccion/' + leccion;
+    },
+    presentarLecciones: function(){
+      var self = this;
+      $.each(self.estudiante.lecciones, function(index, leccion){
+        console.log('Actualmente en la leccion: ');
+        console.log(leccion);
+        if(leccion.leccion!=null){
+          console.log('Anadida la leccion')
+          self.lecciones.push(leccion);
+        }
+        else{
+          console.log('Leccion nula, no anadida')
+        }
+        console.log('-----------------------------')
+      })
     }
   }
 });
