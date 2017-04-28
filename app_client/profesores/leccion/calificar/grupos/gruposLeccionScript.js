506433280f90a91924185676754ebf514bcd5918
jguilindro <julio.guilindro@gmail.com>
2017-04-28 01:57:26 -0500

    generar grupos leccion
    menu para escoger grupos para calificar leccion

---
+++ app_client/profesores/leccion/calificar/grupos/gruposLeccionScript.js
@@ -0,0 +1,86 @@
+var App = new Vue({
+  mounted: function(){
+    $('.button-collapse').sideNav();
+    $(".dropdown-button").dropdown({ hover: false });
+    $('.scrollspy').scrollSpy();
+    $('#modalEliminarPregunta').modal();
+    $('#modalNuevoCapitulo').modal();
+	this.getLeccion();
+	this.getEstudiante();
+  },
+  el: '#app',
+  data: {
+    leccion: {
+      nombre: '',
+      tiempoEstimado: '',
+      tipo: '',
+      fechaInicio: '',
+      preguntas: [],
+      puntaje: 0
+    },
+	preguntas: [],
+	respuestas: [],
+	estudiante: {
+	nombres: '',
+	apellidos: '',
+	correo: ''
+	}
+  },
+  methods: {
+	//carga la lección que se quiere va a calificar con cada pregunta
+	getLeccion: function(){
+	var self = this;
+	var leccionId = window.location.href.toString().split('/')[6];
+	var obtenerLeccionURL = '/api/lecciones/'+ leccionId;
+    this.$http.get(obtenerLeccionURL).then(res => {
+	self.leccion = res.body.datos;
+	leccion= self.leccion;
+	//Saca las preguntas de la base de datos
+	$.each(self.leccion.preguntas, function(index, pregunta){
+	pregunta['show'] = true;
+	 var id= pregunta.pregunta;
+		self.$http.get('/api/preguntas/'+id).then(res => {
+		self.preguntas.push(res.body.datos);
+		
+        })
+		self.getRespuesta(id);
+	});
+        });
+		
+	
+},
+//Obtiene la respuesta del estudiante a dada pregunta
+	getRespuesta: function(preguntaId){
+	var self = this;
+	var leccionId = window.location.href.toString().split('/')[6];
+	var estudianteId = window.location.href.toString().split('/')[7];
+	var obtenerRespuestaURL = '/api/respuestas/buscar/leccion/'+ leccionId+ '/pregunta/'+preguntaId+ '/estudiante/'+estudianteId;
+    this.$http.get(obtenerRespuestaURL).then(res => {
+		self.respuestas.push(res.body.datos);
+		
+	});
+	},
+	
+	getEstudiante: function(){
+	var estudianteId = window.location.href.toString().split('/')[7];
+	var obtenerEstudianteURL = '/api/estudiantes/' + estudianteId;
+	this.$http.get(obtenerEstudianteURL).then(res => {
+		App.estudiante.nombres=res.body.datos.nombres;
+		App.estudiante.apellidos=res.body.datos.apellidos;
+		App.estudiante.correo=res.body.datos.correo;
+		console.log(App.estudiante);
+	});
+	}
+	
+      }
+});
+
+function regresar(){
+	window.location.href = '/profesores/grupos/'
+}
+
+function enviarFeedback(){
+	window.location.href = '/profesores/grupos/'
+}
+// document.getElementById('datePicker').valueAsDate = new Date();
+// document.getElementById('datePicker').setAttribute('min', "2017-04-09")
