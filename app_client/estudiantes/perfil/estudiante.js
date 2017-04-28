02afd400f93910cf58b722a6744c5ac4a6ed387e
edisonmora95 <edison_andre_9@hotmail.com>
2017-04-27 20:18:47 -0500

    Minor fixes al perfil de estudiante

--- app_client/estudiantes/perfil/estudiante.js
+++ app_client/estudiantes/perfil/estudiante.js
@@ -8,39 +8,31 @@ var app = new Vue({
     lecciones: []
   },
   methods: {
-    tomarLeccion : function(){
-        window.location.href = '/estudiantes/tomar-leccion'
-    },
     obtenerLogeado: function() {
       var self = this;
       this.$http.get('/api/session/usuario_conectado').
         then(res => {
           if (res.body.estado) {
             self.estudiante = res.body.datos;
-            //console.log('Este es el estudiante conectado')
-            //console.log(self.estudiante);
             self.presentarLecciones();
           }
         });
+    }, 
+    presentarLecciones: function(){
+      var self = this;
+      $.each(self.estudiante.lecciones, function(index, leccion){
+        //Existía un bug que mostraba lecciones sin referencia a la lección que pertenecía. Esta validación es para no mostrar eso
+        if(leccion.leccion!=null){
+          self.lecciones.push(leccion);
+        }
+      })
+    },
+    tomarLeccion : function(){
+        window.location.href = '/estudiantes/tomar-leccion'
     },
     verLeccion: function(event){
       leccion = event.currentTarget.id;
       window.location.href = '/estudiantes/ver-leccion/' + leccion;
-    },
-    presentarLecciones: function(){
-      var self = this;
-      $.each(self.estudiante.lecciones, function(index, leccion){
-        //console.log('Actualmente en la leccion: ');
-        //console.log(leccion);
-        if(leccion.leccion!=null){
-          //console.log('Anadida la leccion')
-          self.lecciones.push(leccion);
-        }
-        else{
-          //console.log('Leccion nula, no anadida')
-        }
-        //console.log('-----------------------------')
-      })
     }
   }
 });
