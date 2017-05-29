7feb605b6dfbb8d8f921ec40fb9ecf56e9974396
Joel Rodriguez <joelerll@gmail.com>
2017-05-29 13:46:27 -0500

    Cambiado socketio a bower

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ if (os.hostname() === 'joelerll-laptop') {
 
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server, {'pingInterval': 4000, 'pingTimeout': 8000});
+var io = require('socket.io')(server, {'pingInterval': 1000, 'pingTimeout': 5000});
+io.set('heartbeat interval', 1)
 
 app.use(function(req, res, next){
   res.io = io;
