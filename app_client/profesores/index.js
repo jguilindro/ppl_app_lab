314b3103b984aeaa5d485a8c3a006a30afdcb5a8
edanmora95 <edison_andre_9@hotmail.com>
2017-06-23 11:56:23 -0500

    Implementación de algoritmo de compresión

--- app_client/profesores/index.js
+++ app_client/profesores/index.js
@@ -8,3 +8,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   })
 });
+
+function getImage(input) {
+  console.log(input.files[0]);
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $('#source_image')
+        .attr('src', e.target.result)
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function comprimir(e){
+  var output_format = "jpg";
+  var source_image = document.getElementById('source_image');
+  var result_image = document.getElementById('result_image');
+  if (source_image.src == "") {
+      alert("You must load an image first!");
+      return false;
+  }
+  var quality = 15;
+
+  result_image.src = jic.compress(source_image,quality,output_format).src;  
+  console.log(result_image);
+  result_image.onload = function(){
+    var image_width=$(result_image).width(),
+      image_height=$(result_image).height();
+          
+    if(image_width > image_height){
+      result_image.style.width="320px";
+    }else{
+      result_image.style.height="300px";
+    }
+   result_image.style.display = "block";
+
+
+  }
+}
+
