7ad763cbbf77f2b284b12f5206e281591a0dca83
Xidrovo <xidrovo@hotmail.com>
2017-08-19 18:17:30 -0500

    front-end rubrica

---
+++ app_client/profesores/rubrica/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  $.get({
+    url: "/../navbar/profesores",
+    success: function(data) {
+      document.getElementById('#navbar').innerHTML = data;
+      $(".button-collapse").sideNav();
+      $(".dropdown-button").dropdown();
+    }
+  })
+});
