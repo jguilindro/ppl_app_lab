6381d3a415423da0113d4a0344ffe774062d1641
edanmora95 <edison_andre_9@hotmail.com>
2017-10-15 16:02:58 -0500

    Fix seleccion grupos al calificar

--- app_client/profesores/leccion/calificar/grupos/gruposLeccionScript.js
+++ app_client/profesores/leccion/calificar/grupos/gruposLeccionScript.js
@@ -1,25 +1,27 @@
 var app = new Vue({
   created: function(){
+    var pathname = window.location.pathname;
+    this.idLeccion = pathname.split('/')[5];
     this.profesorLogeado();
-    this.obtenerRegistrosCalificaciones();
+    this.obtenerRegistrosCalificaciones(this);
   },
   mounted: function(){
-    //Inicializadores de Materialize
-    $('.button-collapse').sideNav();
-    $(".dropdown-button").dropdown({ hover: false });
-    $('select').material_select();
+    this.inicializarMaterialize();
   },
   el: '#app',
   data: {
+      idLeccion: '',
       profesor: {},
       registros: [],    //Registros de calificaciones -> leccion, grupo, nombrGrupo, paralelo, nombreParalelo, participantes, calificada, calificacion
-      grupos: [],
-      registrosAMostrar: [],
       grupoSeleccionado: {},
-      estudiantes: [],
-      estudianteEscogidoId: ''
+      estudianteSeleccionado: {}
 	},
   methods: {
+    inicializarMaterialize: function(){
+      $('.button-collapse').sideNav();
+      $(".dropdown-button").dropdown({ hover: false });
+      $('select').material_select();
+    },
     profesorLogeado: function() {
       var self = this;
       $.get({
@@ -31,102 +33,46 @@ var app = new Vue({
         }
       });
 		},
-    obtenerRegistrosCalificaciones: function(){
-      //Obtiene los registros de las calificaciones de la lección que se quiere calificar
-      var self = this;
-      var pathname = window.location.pathname;
-      var idLeccion = pathname.split('/')[5];
-      var urlApi = '/api/calificaciones/' + idLeccion;
+    //Obtiene los registros de las calificaciones de la lección que se quiere calificar
+    obtenerRegistrosCalificaciones: function(self){
+      var urlApi = '/api/calificaciones/' + self.idLeccion;
       $.get({
         url: urlApi,
         success: function(res){
-          self.registros = res.datos;
-          console.log('Registros obtenidos')
-          self.obtenerTodosGrupos();
+          app.registros = res.datos;
+          console.log('Registros obtenidos', self.registros)
+          //Los ordeno por nombre
+          self.registros.sort(function(r1, r2){
+            var nombreR1 = r1.nombreGrupo;
+            var numeroR1 = parseInt(nombreR1.split(" ")[1]);
+            var nombreR2 = r2.nombreGrupo;
+            var numeroR2 = parseInt(nombreR2.split(" ")[1]);
+            return ((numeroR1 < numeroR2) ? -1 : ((numeroR1 > numeroR2) ? 1 : 0));
+          });
+          //Muestro solo los grupos que no han sido calificados
+          self.registros = $.grep(self.registros, function(registro){
+            return ( registro.calificada == false && registro.participantes.length != 0 );
+          });
         }
       })
     },
-    obtenerTodosGrupos: function(){
-      var self = this;
-      $.get({
-        url: '/api/grupos',
-        success: function(res){
-          self.grupos = res.datos;
-          self.buscarNombreGrupos();
-        }
-      })
+    seleccionGrupo: function(registro){
+      app.grupoSeleccionado = registro;
+      console.log(app.grupoSeleccionado);
     },
-    buscarNombreGrupos: function(){
-      //Esta función añade el nombre de cada grupo del array de registros obtenidos.
-      //Busca el nombre del grupo dentro del array grupos.
-      var self = this;
-      $.each(self.registros, function(index, registro){
-        $.each(self.grupos, function(j, grupo){
-          if(registro.grupo==grupo._id){
-            registro.nombreGrupo = grupo.nombre;
-            return false
-          }
-        });
-      });
-      //Los ordeno por nombre
-      self.registros.sort(function(r1, r2){
-        var nombreR1 = r1.nombreGrupo;
-        var numeroR1 = parseInt(nombreR1.split(" ")[1]);
-        var nombreR2 = r2.nombreGrupo;
-        var numeroR2 = parseInt(nombreR2.split(" ")[1]);
-        return ((numeroR1 < numeroR2) ? -1 : ((numeroR1 > numeroR2) ? 1 : 0));
-      });
-      //Los muestro en este nuevo array
-      self.registrosAMostrar = self.registros;
-
-      //Muestro solo los grupos que no han sido calificados
-      self.registrosAMostrar = $.grep(self.registrosAMostrar, function(registro){
-        return registro.calificada==false&&registro.participantes.length!=0;
-      })
+    seleccionEstudiante: function(estudiante){
+      app.estudianteSeleccionado = estudiante;
+      console.log(app.estudianteSeleccionado);
     },
-    buscarNombreEstudiantes: function(){
-      //Busco el nombre de los estudiantes del grupo seleccionado para calificar para mostrarlos en la pestaña 'Seleccionar estudiantes'
-      var self = this;
-      self.estudiantes = [];  //Tengo que vaciarlo para que si el usuario selecciona un grupo y luego otro entonces no se acumulen los estudiantes en este array
-      $.each(self.grupoSeleccionado.participantes, function(index, estudiante){
-        //Por cada estudiante del grupo seleccionado, hago una llamada a la api para conseguir su nombre.
-        var urlApi = '/api/estudiantes/' + estudiante._id;
-        $.get({
-          url: urlApi,
-          success: function(res){
-            self.estudiantes.push(res.datos);
-          }
-        });
-      });
+    calificarRedireccion: function(){
+      if ( app.estudianteSeleccionado ){
+        window.location.href = '/profesores/leccion/calificar/'+app.idLeccion+'/'+app.estudianteSeleccionado._id+'/'+app.grupoSeleccionado._id;
+      }else{
+        Materialize.toast('Seleccione un estudiante primero!', 3000, 'rounded red');
+      }
     }
 	}
-
 });
-
-function grupoSeleccionado(_element){
-  var self = app.$data
-  console.log(_element.id)
-  $.each(self.registrosAMostrar, function(index, registro){
-    console.log(registro.grupo)
-    if(_element.id==registro.grupo._id){
-      self.grupoSeleccionado = registro;
-      app.buscarNombreEstudiantes();
-      return false
-    }
-  })
-}
-
-function estudianteSeleccionado(_element){
-  var self = app.$data
-  self.estudianteEscogidoId = _element.id;
-}
-
-function calificarRedireccion(){
-	if (app.estudianteEscogidoId){
-  	var leccionId = window.location.href.toString().split('/')[7];
-  	window.location.href = '/profesores/leccion/calificar/'+leccionId+'/'+app.estudianteEscogidoId+'/'+app.grupoSeleccionado.grupo._id;
-	}
-}
 
 function regresar(){
 	window.location.href = '/profesores/grupos/'
