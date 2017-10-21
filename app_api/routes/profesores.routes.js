66b7b129f57e46fe1a24aedea06c852567acf09f
Joel Rodriguez <joelerll@gmail.com>
2017-10-21 15:06:55 -0500

    add apidoc url ejemplos

--- app_api/routes/profesores.routes.js
+++ app_api/routes/profesores.routes.js
@@ -1,9 +1,24 @@
 //http://some.api.com/posts/comments?fields=name,image.
 let ProfesorController = require('../controllers/profesores.controller')
 
+
+// setear que si es development el auth debe de ser libre, si es production no
 module.exports = (app) => {
   const profesorController = new ProfesorController()
   app.route('/profesores')
+    /**
+      * @api {get} /api/profesores Obtener profesores
+      * @apiName ObtenerProfesores
+      * @apiGroup Profesores
+      * @apiPermission Profesores Admin
+      * @apiDescription Todos los Profesores
+      *
+      * @apiSuccess {Number} id
+      * @apiSuccess {String} nombres
+      * @apiSuccess {String} apellidos
+      * @apiSuccess {String} correo
+      * @apiSampleRequest /profesores
+    **/
     .get((req, res) => {
       profesorController.obtenerTodosProfesores().then(function(respuesta) {
         res.status(respuesta.codigo_estado)
